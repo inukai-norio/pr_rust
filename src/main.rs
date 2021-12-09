3bd20978e274f5358c29953678cdfc7ba835a336
jj1uty <jj1uty@jarl.com>
2021-12-09 22:54:23 +0900

    力技

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 use std::env;
-use regex::Regex;
 
 fn main() {
-    for (key, value) in env::vars() {
-        //if Regex::new(r"^CARGO_PKG_").unwrap().is_match(&key) {
-            println!("{}: {}", key, value);
-        //}
-    }
+    println!("{: <30}: {}", "CARGO"                  , env!("CARGO"));
+    println!("{: <30}: {}", "CARGO_HOME"             , env!("CARGO_HOME"));
+    println!("{: <30}: {}", "CARGO_MANIFEST_DIR"     , env!("CARGO_MANIFEST_DIR"));
+    println!("{: <30}: {}", "CARGO_PKG_AUTHORS"      , env!("CARGO_PKG_AUTHORS"));
+    println!("{: <30}: {}", "CARGO_PKG_DESCRIPTION"  , env!("CARGO_PKG_DESCRIPTION"));
+    println!("{: <30}: {}", "CARGO_PKG_HOMEPAGE"     , env!("CARGO_PKG_HOMEPAGE"));
+    println!("{: <30}: {}", "CARGO_PKG_LICENSE"      , env!("CARGO_PKG_LICENSE"));
+    println!("{: <30}: {}", "CARGO_PKG_LICENSE_FILE" , env!("CARGO_PKG_LICENSE_FILE"));
+    println!("{: <30}: {}", "CARGO_PKG_NAME"         , env!("CARGO_PKG_NAME"));
+    println!("{: <30}: {}", "CARGO_PKG_REPOSITORY"   , env!("CARGO_PKG_REPOSITORY"));
+    println!("{: <30}: {}", "CARGO_PKG_VERSION"      , env!("CARGO_PKG_VERSION"));
+    println!("{: <30}: {}", "CARGO_PKG_VERSION_MAJOR", env!("CARGO_PKG_VERSION_MAJOR"));
+    println!("{: <30}: {}", "CARGO_PKG_VERSION_MINOR", env!("CARGO_PKG_VERSION_MINOR"));
+    println!("{: <30}: {}", "CARGO_PKG_VERSION_PATCH", env!("CARGO_PKG_VERSION_PATCH"));
+    println!("{: <30}: {}", "CARGO_PKG_VERSION_PRE"  , env!("CARGO_PKG_VERSION_PRE"));
 
     return;
 }
