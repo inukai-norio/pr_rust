d959e0674894f4f4760548a4f743c4978ea2c2bd
jj1uty <jj1uty@jarl.com>
2021-12-09 22:17:55 +0900

    CARGO 関連のenvを表示させる

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    for (key, value) in env::vars() {
+        if Regex::new(r"^CARGO_PKG_").unwrap().is_match(&key) {
+            println!("{}: {}", key, value);
+        }
+    }
+
+    return;
 }
